f5e3062b8ccc1af20def87f49cf9102d98668971
Alex Shumakov <shumalexonly@ya.ru>
2018-01-31 01:06:49 +0700

    RES-332 Add DishModalheader.

---
+++ Restomania.App.Kuzina/Restomania.App.Kuzina/UI/Controllers/Place/DishModals/Elements/Header/DishModalHeader.swift
@@ -0,0 +1,77 @@
+//
+//  DishModalHeader.swift
+//  RestomaniaAppKuzina
+//
+//  Created by Алексей on 31.01.18.
+//  Copyright © 2018 Medved-Studio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import IOSLibrary
+
+public class DishModalHeader: UITableViewCell {
+
+    private static let nibName = "\(String.tag(DishModalHeader.self))View"
+    public static func create(for dish: BaseDish, from menu: MenuSummary) -> DishModalHeader {
+
+        let cell: DishModalHeader = UINib.instantiate(from: nibName, bundle: Bundle.main)
+        cell.update(by: dish, from: menu)
+
+        return cell
+    }
+
+    //UI
+    @IBOutlet private weak var imageContainerView: UIView!
+    private var imageContainerHeight: NSLayoutConstraint?
+    @IBOutlet private weak var dishImage: ImageWrapper!
+    @IBOutlet private weak var nameContainerView: UIView!
+    @IBOutlet private weak var nameLabel: UILabel!
+
+    //Data
+    private var dish: BaseDish? = nil {
+        didSet {
+            apply()
+        }
+    }
+    private var delegate: DishModalDelegateProtocol?
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+
+        nameLabel.font = ThemeSettings.Fonts.bold(size: .title)
+        nameLabel.textColor = ThemeSettings.Colors.main
+    }
+    private func apply() {
+        guard let dish = self.dish else {
+            return
+        }
+
+        imageContainerHeight?.constant = String.isNullOrEmpty(dish.image) ? 0.0 : 150.0
+        dishImage.setup(url: dish.image)
+
+        nameLabel.text = dish.name
+    }
+
+    @IBAction private func closeModal() {
+        delegate?.closeModal()
+    }
+}
+extension DishModalHeader: DishModalElementsProtocol {
+
+    public func link(with delegate: DishModalDelegateProtocol) {
+        self.delegate = delegate
+    }
+    public func update(by dish: BaseDish, from: MenuSummary) {
+        self.dish = dish
+    }
+}
+extension DishModalHeader: InterfaceTableCellProtocol {
+
+    public var viewHeight: Int {
+        return Int(nameContainerView.frame.height + (imageContainerHeight?.constant ?? 0.0))
+    }
+    public func prepareView() -> UITableViewCell {
+        return self
+    }
+}
